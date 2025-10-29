a0fe63a6d76fc8cc18b4ca2d9a1c9c0d07063aef
corechunk <mhmiraj2005@gmail.com>
2025-10-30 01:07:45 +0600

    more

--- essential.sh
+++ essential.sh
@@ -1,20 +1,40 @@
 apps_cli_dev=(
+    git
     build-essential   # essential build tools (gcc, g++, make)
+    gdb
+    manpages-dev
     make               # GNU make utility
     ninja              # fast build system
     cmake              # cross-platform build tool for c++
-    openjdk-25-jdk     # Java Development Kit
-    openjdk-25-jre     # Java runtime
+    openjdk-25-jdk     # Java Development Kit #contains: openjdk-25-jre # Java runtime
     python3
+    python3-pip
 )
 
+network_tools_cli=(
+    wget
+    net-tools
+    nmap
+    iwd
+    ufw
+    fail2ban
+)
 
 core_cli=(
     tmux               # terminal multiplexer
     neovim             # cli code editor [standard]
     nano               # cli text editor [standard]
     mpv                # cli media playback [gui will work in Desktop Environments]
-    btop    # System Observer [ like: taskmanager ] > even tho idk if management is possible
+    btop               # System Observer [ like: taskmanager ] > even tho idk if management is possible
+    fastfetch
+    zip
+    unzip
+    bat
+    lsd
+    zoxide
+    fzf
+    ripgrep
+    fonts-firacode
 )
 
 core_gui=(
@@ -22,10 +42,11 @@ core_gui=(
     thunar       # nice & compatible file manager
     mousepad     # text editor [like notepad from windows]
     mpv          # media playback audio/video
-    waybar       # taskbar type thing
-    rofi
     obs-studio   # screen recorder/streamer [free]
     shotcut      # video editor [free]
+    waybar       # taskbar type thing
+    rofi
+    xdg-utils    # cli [for all Desktop Environment]
 )
 
 github_apps=(
